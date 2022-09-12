ee5fcbcdeb7452af985996090066f693b8baac60
Yankai Chen <78847382+huabrother@users.noreply.github.com>
2022-09-13 01:34:52 +1000

    1

--- src/test/java/NormalUserTest.java
+++ src/test/java/NormalUserTest.java
@@ -1,4 +1,10 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,3 +42,81 @@ class NormalUserTest {
         assertEquals(normalUser.currencyList.get(3).getName(), "EUR");
     }
 }
+
+
+
+//import org.junit.Before;
+//        import org.junit.jupiter.api.AfterEach;
+//        import org.junit.jupiter.api.BeforeEach;
+//        import org.junit.jupiter.api.Test;
+//        import java.io.ByteArrayOutputStream;
+//        import java.io.PrintStream;
+//        import java.util.ArrayList;
+//
+//        import java.io.PrintStream;
+//
+//        import static org.junit.jupiter.api.Assertions.*;
+//
+//class NormalUserTest {
+//
+//    User NormalUser;
+//
+//    private final PrintStream standardOut = System.out;
+//    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+//
+//    @BeforeEach
+//    void setUp() {
+//        this.NormalUser = new NormalUser();
+//        System.setOut(new PrintStream(outputStreamCaptor));
+//
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//        this.NormalUser = null;
+//    }
+//
+//    @Test
+//    void addCurrency() {
+//        User normalUser = new NormalUser();
+//        int currencyListLengthA = User.currencyList.size();
+//        assertEquals(currencyListLengthA, 0);
+//
+//
+//    }
+//
+//    @Test
+//    void changeRateTest() {
+//        // Test invalid change
+//        User normalUser = new NormalUser();
+//        normalUser.changeRate("AUD", "XYZ", "50", "2022/12/25");
+//        assertEquals("No such a currency, please add it into system", outputStreamCaptor.toString()
+//                .trim());
+//
+//    }
+//
+////    @Test
+////    void choosePopularTest() {
+////        User normalUser = new NormalUser();
+////        normalUser.choosePopular("AUD", "USD","40" ,"2022/12/25");
+////        assertEquals("AUD", outputStreamCaptor.toString()
+////                .trim());
+////    }
+//
+//
+//
+//    @Test
+//    void choosePopularTest() {
+//        User NormalUser = new NormalUser();
+//        NormalUser.addCurrency("CNY");
+//        NormalUser.addCurrency("AUD");
+//        NormalUser.addCurrency("USD");
+//        NormalUser.addCurrency("EUR");
+//        NormalUser.choosePopular("CNY", "AUD", "USD", "EUR");
+//        assertEquals("CNY", NormalUser.popularCurrencies[0].getName());
+//
+//        // tear down
+//        NormalUser.currencyList = new ArrayList<>();
+//        NormalUser.popularCurrencies = new Currency[4];
+//    }
+//}
